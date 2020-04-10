7795c060487ab323aa7261c30ad7e408a81f3f13
Yaroslav Nedilko <yavned@gmail.com>
2020-04-10 16:36:01 +0200

    clean-up

--- BlazorApp1/Models/BudgetItem.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorApp1.Models
-{
-    public class BudgetItem
-    {
-        public string Name { get; set; }
-        public decimal Amount { get; set; }
-    }
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# blazor-snippets
-Just a Hello word Blazor app with some basic stuff
